1811b9b6f501fa0aab580d2cedd87a329d8275a2
Tolosa Kebede <tolosakbd@gmail.com>
2022-04-07 11:40:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - print using puts() function
+ *
+ * Return: 0 on succesful
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

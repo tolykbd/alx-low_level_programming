6e3a2e5dc1041f27cef6e7c478601b99df8ed38e
Tolosa Kebede <tolosakbd@gmail.com>
2022-04-07 11:56:03 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints using printf function
+ *
+ * Return: 0 on successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

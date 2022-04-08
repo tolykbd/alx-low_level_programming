e10edac26a2a602eb0c3d1384908877d1dca375d
Tolosa Kebede <tolosakbd@gmail.com>
2022-04-08 11:59:37 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print all the letters except q and e
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int ch;
+	ch = 97;
+	while (ch < 123)
+	{
+		if (ch != 113 && ch != 101)
+			putchar(ch);
+		ch++;
+	}
+	return (0);
+}

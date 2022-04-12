9da55d72a60e1016997769fa5af5c7c1c3f98372
Tolosa Kebede <tolosakbd@gmail.com>
2022-04-12 11:46:45 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,59 +6,34 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int a, b, c, d, check;
 
 	a = 0;
+	check = 10;
 	while (a < 3)
 	{
-		if (a == 2) 
+		if (a == 2)
+			check = 3;
+		b = 0;
+		while (b < check)
 		{
-			b = 0;
-			while (b < 4)
+			c = 0;
+			while (c < 6)
 			{
-				c = 0;
-				while (c < 6)
+				d = 0;
+				while (d < 10)
 				{
-					d = 0;
-					while (d < 10)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(':');
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-						d++;
-					}
-					c++;
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(':');
+					_putchar(c + '0');
+					_putchar(d + '0');
+					_putchar('\n');
+					d++;
 				}
-				b++;
+				c++;
 			}
-		}
-		else
-		{
-		
-			b = 0;
-			while (b < 10)
-			{
-				c = 0;
-				while (c < 6)
-				{
-					d = 0;
-					while (d < 10)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(':');
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-						d++;
-					}
-					c++;
-				}
-				b++;
-			}
+			b++;
 		}
 		a++;
 	}

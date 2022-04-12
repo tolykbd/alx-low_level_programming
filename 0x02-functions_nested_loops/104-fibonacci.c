e13112dfab989ff53d3f15cac4f83786fd46883d
Tolosa Kebede <tolosakbd@gmail.com>
2022-04-12 12:59:36 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,29 +1,4 @@
 #include"main.h"
-
-/**
- * numLength - returns the length of string
- *
- * @num: operand number
- *
- * Return: number of digits
-*/
-
-int numLength(int num)
-{
-	int length = 0;
-
-	if (!num)
-		return (1);
-
-	while (num)
-	{
-		num = num / 10;
-		length += 1;
-	}
-
-	return (length);
-}
-
 /**
  * main - Entry point
  *

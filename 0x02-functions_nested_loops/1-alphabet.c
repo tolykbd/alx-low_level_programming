365e43780c7842977980ba8c8300622609299268
Tolosa Kebede <tolosakbd@gmail.com>
2022-04-12 07:01:54 -0700

    prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,23 @@
 #include "main.h"
+/**
+ * print_alphabet - prints a-z latters
+ *
+ * Return: void;
+ */
+void print_alphabet(void)
+{
+	int chr;
 
+	chr = 97;
+	while (chr < 123)
+	{
+		_putchar(chr);
+		chr++;
+	}
+	_putchar('\n');
+
+	return;
+}
 /**
  * main - check the code
  *

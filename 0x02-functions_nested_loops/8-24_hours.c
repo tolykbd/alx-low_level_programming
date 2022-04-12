02dd94d765722a3ac770d5c42d6b0cfe7bb688fb
Tolosa Kebede <tolosakbd@gmail.com>
2022-04-12 11:30:09 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,26 +11,54 @@ void jack_bauer(void)
 	a = 0;
 	while (a < 3)
 	{
-		b = 0;
-		while (b < 4)
+		if (a == 2) 
 		{
-			c = 0;
-			while (c < 6)
+			b = 0;
+			while (b < 4)
 			{
-				d = 0;
-				while (d < 10)
+				c = 0;
+				while (c < 6)
 				{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(':');
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
-					d++;
+					d = 0;
+					while (d < 10)
+					{
+						_putchar(a + '0');
+						_putchar(b + '0');
+						_putchar(':');
+						_putchar(c + '0');
+						_putchar(d + '0');
+						_putchar('\n');
+						d++;
+					}
+					c++;
 				}
-				c++;
+				b++;
 			}
-			b++;
+		}
+		else
+		{
+		
+			b = 0;
+			while (b < 10)
+			{
+				c = 0;
+				while (c < 6)
+				{
+					d = 0;
+					while (d < 10)
+					{
+						_putchar(a + '0');
+						_putchar(b + '0');
+						_putchar(':');
+						_putchar(c + '0');
+						_putchar(d + '0');
+						_putchar('\n');
+						d++;
+					}
+					c++;
+				}
+				b++;
+			}
 		}
 		a++;
 	}

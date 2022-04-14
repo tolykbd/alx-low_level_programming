8857121810ba6b96e64929f6e5d30c8ff9a93751
Tolosa Kebede <tolosakbd@gmail.com>
2022-04-14 11:07:15 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_diagonal - draws a diagonal line on the terminal
- * @n: number of times the character \ should be printed
+ * print_square - draws a diagonal line on the terminal
+ * @size: number of times the character \ should be printed
  */
 void print_square(int size)
 {

fc6a31bd714ad95dfc7b0b58557995985e1d0fe8
Tolosa Kebede <tolosakbd@gmail.com>
2022-04-18 08:53:33 -0700

    prints a string followd by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _puts - prints string
+* @str: string to be printed
+* Return: nothing
+*/
+void _puts(char *str)
+{
+	int c;
+
+	c = 0;
+	while(str[c] != '\0')
+	{
+		_putchar(str[c]);
+		c++;
+	}
+}

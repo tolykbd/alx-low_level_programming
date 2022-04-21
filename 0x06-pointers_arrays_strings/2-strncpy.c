25d39358bb5ff900bcc29fe04fc0ac71084d8ab5
Tolosa Kebede <tolosakbd@gmail.com>
2022-04-21 10:18:35 -0700

    task 2 concatnate strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
-* _strncpy - concatnate two strings
-* @dest: first string to be appended
-* @src: second string
-* @n: number of bytes
-* Return: concatnate string
-*/
+ *_strncpy - copy a string
+ *@dest: destination string
+ *@src: source of string
+ *@n: max byte of src
+ *Return: dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i = 0;
+	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-	}
-	for (j = 0; j < n; j++)
-	{
+	while (src[i] != '\0')
+		i++;
+	for (j = 0; j < n && src[j] != '\0'; j++)
 		dest[j] = src[j];
-	}
+	for (; n > i; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }

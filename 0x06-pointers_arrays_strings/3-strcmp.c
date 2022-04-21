876c5c1f927360e54c4722c3b14de7c4496554c0
Tolosa Kebede <tolosakbd@gmail.com>
2022-04-21 12:23:12 -0700

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,7 @@ int _strcmp(char *s1, char *s2)
 	int i, j, k;
 
 	j = 0;
-	for (i = 0; s1[i] != '\0' && s2[j] != '\0' ; i++)
+	for (i = 0; s1[i] != '\0' || s2[j] != '\0' ; i++)
 	{
 		k = s1[i] - s2[j];
 		if (s1[i] != s2[j])
@@ -19,4 +19,6 @@ int _strcmp(char *s1, char *s2)
 		}
 		j++;
 	}
+
+	return (0);
 }

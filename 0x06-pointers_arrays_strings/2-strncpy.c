1b96c84eee5e00d882d16e03f71638a7fe2ab69d
Tolosa Kebede <tolosakbd@gmail.com>
2022-04-21 10:07:36 -0700

    task 2 concatnate strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; j < n && src[j] != '\0'; j++)
+	for (j = 0; j < n; j++)
 	{
 		dest[j] = src[j];
 	}

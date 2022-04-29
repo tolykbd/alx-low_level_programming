b58a2f0278da6265138bb9ff0d630da7fde07d59
tolykbd <tolosakbd@gmail.com>
2022-04-29 12:51:31 -0700

    prints the arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - shows the arguments
+ * @argc: number of arguments
+ * @argv: argumets srting
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 0)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s", argv[i]);
+		}
+		printf("\n");
+	}
+	return (0);
+}

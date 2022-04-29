5f9f57433c3215a33926fce0c7638e8dc5d40057
tolykbd <tolosakbd@gmail.com>
2022-04-29 14:28:22 -0700

    multilication of two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
-  * main - main
-  * @argc: argumetns num
-  * @arcv: argumetns
-  * Return: 0 on successs 1 otherwise
-  */
-int main(int argc, char *arcv[])
-{
-	int i;
 
-	if (argc > 1)
+/**
+  * main - prints its name, followed by new.
+  * @argc: argument counter.
+  * @argv: argument vector.
+  * Return: 0, if error return 1.
+  */
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc > 1 && (argc - 1) == 2)
 	{
-		i = atoi(arcv[1]) * atoi(arcv[2]);
-		printf("%d\n", i);
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-
 	return (0);
 }

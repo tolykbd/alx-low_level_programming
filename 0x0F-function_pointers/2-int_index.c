76212a2e76ecc8846e6ecfc3e5b4c164ec58551d
tolykbd <tolosakbd@gmail.com>
2022-05-11 13:50:44 -0700

    find numbers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,13 +4,12 @@
 * @array: array of intergers
 * @size: length of array
 * @cmp: function pointer
+* Return: 1 on success -1 otherwise
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
 	if (array && cmp)
 	{
 		for (i = 0; i < size; i++)

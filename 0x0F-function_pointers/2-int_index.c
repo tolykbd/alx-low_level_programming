c297d344ee128d61dba7d8bf3617c7971ffea9cf
tolykbd <tolosakbd@gmail.com>
2022-05-11 13:39:17 -0700

    find numbers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
 * int_index - prints name
 * @array: array of intergers
@@ -13,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) == 1)
+		if (cmp(array[i]) != 0)
 			return (i);
 	}
 

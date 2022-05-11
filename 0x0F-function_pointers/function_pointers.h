594c833769fc6f8a76c7de30907cd5eb2e039be1
tolykbd <tolosakbd@gmail.com>
2022-05-11 13:07:04 -0700

    rint array of numbers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
 #ifndef FUNC_POINT_H
 #define FUNC_POINT_H
+
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));

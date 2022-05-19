82789cf5e753bc5516e010435b68af7941e6321b
tolykbd <tolosakbd@gmail.com>
2022-05-19 13:09:20 -0700

    print lenght and print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,9 +11,9 @@ nelem = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
-printf("[%d] %s\n", 0, "(nil)");
+printf("[%lu] %s\n", 0, "(nil)");
 else
-printf("[%d] %s\n", h->len, h->str);
+printf("[%lu] %s\n", h->len, h->str);
 h = h->next;
 nelem++;
 }

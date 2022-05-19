a6f740ea570b0f502fc18c3d2474c33da91f9cb3
tolykbd <tolosakbd@gmail.com>
2022-05-19 13:16:25 -0700

    print length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,16 +4,15 @@
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
-
 size_t list_len(const list_t *h)
 {
-    size_t nelem;
+	size_t nelem;
 
-    nelem = 0;
-    while (h != NULL)
-    {
-        h = h->next;
-        nelem++;
-    }
-    return (nelem);
+	nelem = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
 }

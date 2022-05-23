67c313bf2e3de42cbdb63164817f88aab25c069e
tolykbd <tolosakbd@gmail.com>
2022-05-23 14:17:42 -0700

    find node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - get node at the index
+ * @head: node list
+ * @index: index to be found
+ * Return: the index node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+	}
+	if (i == index)
+		return (head);
+	else
+		return (NULL);
+}

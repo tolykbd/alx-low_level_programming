afb0edcaca2c97841c47c5ea0a4d6c0f93ac0ace
tolykbd <tolosakbd@gmail.com>
2022-05-23 16:20:18 -0700

    add node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp->n = n;
 			temp->next = tempHead->next;
 			tempHead->next = temp;
-			return (tempHead);
+			return (temp);
 		}
 		i++;
 		tempHead = tempHead->next;

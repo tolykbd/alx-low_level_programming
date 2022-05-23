1d331bb76788a395c3c324ccf3081a8849bc10be
tolykbd <tolosakbd@gmail.com>
2022-05-23 06:53:06 -0700

    free node 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint2 - free node
+ * @head: node list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while ((current = *head) != NULL)
+	{
+		*head = (*head)->next;
+		free(current);
+	}
+}
